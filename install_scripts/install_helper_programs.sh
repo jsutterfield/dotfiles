b87148aa98b63555eb67535b13b9d2286d83b856
James Sutterfield <jsutterfield@uber.com>
2023-04-17 17:13:13 +0200

    Update install helper script

--- install_scripts/install_helper_programs.sh
+++ install_scripts/install_helper_programs.sh
@@ -4,9 +4,7 @@ set -euxo pipefail
 
 # Function to install packages on Ubuntu
 install_on_ubuntu() {
-  sudo apt update
-  sudo apt install -y fzf ripgrep fd-find broot tldr bat
-  sudo ln -s "$(command -v fdfind)" /usr/local/bin/fd
+  echo "install these programs manually, or update the script to make it work better: fzf ripgrep fd-find broot tldr bat"
 }
 
 # Function to install packages on MacOS
